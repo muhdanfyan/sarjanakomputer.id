c182be7c4a172b163e1c20069dd33f4010efce91
Muhdan Fyan Syah Sofian <muhdan.fyan@student.iou.edu.gm>
2026-05-20 22:22:22 +0700

    fix: pindahkan api/ ke html/api/ biar Vercel serve serverless function

--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const token = process.env.DECAP_CMS_TOKEN;
-  res.json({
-    hasToken: !!token,
-    tokenPrefix: token ? token.substring(0, 5) + '...' : null,
-    tokenLength: token ? token.length : 0,
-    nodeEnv: process.env.NODE_ENV
-  });
-}
